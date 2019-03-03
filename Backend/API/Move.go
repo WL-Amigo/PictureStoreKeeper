c92a3c7d30d88fb471c5613fbc03495f22b8b70e
Amigo(WhiteLuckers) <white.luckers@gmail.com>
2019-03-03 15:17:57 +0900

    Backend/Frontend: add delete picture API endpoint

--- Backend/API/Move.go
+++ Backend/API/Move.go
@@ -16,6 +16,11 @@ type moveFilePayload struct {
 	FileName       string `json:"file_name"`
 }
 
+type deleteFilePayload struct {
+	SourceDirIndex int `json:"source_dir_index"`
+	FileName       string `json:"file_name"`
+}
+
 func AddMoveEndpoints(e *echo.Echo, albumManager *Model.AlbumManager) {
 	moveEndpoint := e.Group("/api/move/:albumId")
 
@@ -35,20 +40,54 @@ func AddMoveEndpoints(e *echo.Echo, albumManager *Model.AlbumManager) {
 		albumPublic := &album.AlbumPublic
 		sourceDirIndex := payload.SourceDirIndex
 		destDirIndex := payload.DestDirIndex
-		if sourceDirIndex < 0 || destDirIndex >= len(albumPublic.DirEntries) {
+		if sourceDirIndex < 0 || sourceDirIndex >= len(albumPublic.DirEntries) {
 			return returnInvalidDirIndexResponse(ctx, strconv.Itoa(sourceDirIndex))
 		}
 		if destDirIndex < 0 || destDirIndex >= len(albumPublic.DirEntries) {
 			return returnInvalidDirIndexResponse(ctx, strconv.Itoa(destDirIndex))
 		}
 
-		// move file arranged directory to source
+		// move file from source directory to dest
 		sourceFullPath := filepath.Join(albumPublic.DirEntries[sourceDirIndex].FullPath, payload.FileName)
 		destFullPath := filepath.Join(albumPublic.DirEntries[destDirIndex].FullPath, payload.FileName)
 		if err := os.Rename(sourceFullPath, destFullPath); err != nil {
 			log.Error(err)
 			return ctx.String(http.StatusInternalServerError, "error occurred when moving file")
 		}
+		return ctx.NoContent(http.StatusOK)
+	})
+
+	moveEndpoint.POST("/delete", func(ctx echo.Context) error {
+		payload := new(deleteFilePayload)
+		if err := ctx.Bind(payload); err != nil {
+			return returnInvalidPayloadResponse(ctx)
+		}
+
+		albumId := ctx.Param(albumIdParamName)
+		album := albumManager.Get(albumId)
+		if album == nil {
+			return returnAlbumNotFoundResponse(ctx)
+		}
+
+		albumPublic := &album.AlbumPublic
+		sourceDirIndex := payload.SourceDirIndex
+		if sourceDirIndex < 0 || sourceDirIndex >= len(albumPublic.DirEntries) {
+			return returnInvalidDirIndexResponse(ctx, strconv.Itoa(sourceDirIndex))
+		}
+
+		// move file from source directory to directory for deleted
+		sourceFullPath := filepath.Join(albumPublic.DirEntries[sourceDirIndex].FullPath, payload.FileName)
+		destFullPath := filepath.Join(albumPublic.WillBeDeletedDir, payload.FileName)
+		if err := os.Rename(sourceFullPath, destFullPath); err != nil {
+			log.Error(err)
+			return ctx.String(http.StatusInternalServerError, "error occurred when moving file")
+		}
+
+		// record source directory label for deleted file
+		albumInternal := &album.AlbumInternal
+		albumInternal.MapFileNameToLabelWhenDeleted[payload.FileName] = albumPublic.DirEntries[sourceDirIndex].Label
+		albumManager.UpdateOrInsert(albumId, album)
+
 		return ctx.NoContent(http.StatusOK)
 	})
 }
