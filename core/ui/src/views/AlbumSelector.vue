d7b45d5764bade8cfffd6e28161d3f92cb6c7d96
Amigo(WhiteLuckers) <white.luckers@gmail.com>
2021-01-23 16:43:40 +0900

    refs #24 rewrite AlbumSelector with Tailwind

--- core/ui/src/views/AlbumSelector.vue
+++ core/ui/src/views/AlbumSelector.vue
@@ -1,35 +1,54 @@
-<template lang="pug">
-section.section
-  .container
-    entity(v-for="iaap in idAndLabelPairs" :key="iaap.id" :label="iaap.label" @click.native="onEntityClicked(iaap.id)")
-    .fullwidth-button.add-button(@click="isCreateAlbumPromptActive = true")
-      span.icon: i.fas.fa-plus-circle
-      span 追加する
-  b-modal(:active.sync="isCreateAlbumPromptActive" has-modal-card)
-    create-album-prompt(@create-album="onCreateAlbumPromptConfirmed")
+<template>
+  <div class="container mx-auto py-8 px-2">
+    <div class="flex flex-col space-y-2">
+      <entity v-for="iaap in idAndLabelPairs" :key="iaap.id" :label="iaap.label" @selected="onEntityClicked(iaap.id)" />
+    </div>
+    <div class="flex flex-row justify-center w-full pt-4">
+      <button
+        class="flex flex-row items-center justify-center text-xl w-1/2 bg-primary-800 text-white p-2 rounded"
+        @click="isCreateAlbumPromptActive = true"
+      >
+        <add-filled class="mr-2 w-5 h-5" />
+        <span>追加する</span>
+      </button>
+    </div>
+    <fade-transition>
+      <div
+        class="z-modal fixed inset-0 bg-black bg-opacity-50 w-screen h-screen flex flex-row items-center justify-center"
+        v-if="isCreateAlbumPromptActive"
+        @click="isCreateAlbumPromptActive = false"
+      >
+        <create-album-prompt @create-album="onCreateAlbumPromptConfirmed" @cancel="isCreateAlbumPromptActive = false" />
+      </div>
+    </fade-transition>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Inject } from "vue-property-decorator";
-import Entity from "../components/AlbumSelectorEntity.vue";
-import CreateAlbumPrompt from "../components/CreateAlbumPrompt.vue";
-import { AlbumAPIService } from "@/services/AlbumAPIService";
-import { IDAndLabelPair } from "@/models/IDAndLabelPair";
-import { DialogConfig } from "buefy/types/components";
+import { Component, Vue, Inject } from 'vue-property-decorator';
+import Entity from '../components/AlbumSelectorEntity.vue';
+import CreateAlbumPrompt from '../components/CreateAlbumPrompt.vue';
+import { AlbumAPIService } from '@/services/AlbumAPIService';
+import { IDAndLabelPair } from '@/models/IDAndLabelPair';
+import { DialogConfig } from 'buefy/types/components';
+import AddFilledIcon from '@/components/icons/AddFilled.vue';
+import FadeTransition from '@/components/transitions/Fade.vue';
 
 const CreateAlbumPromptDefinition: DialogConfig & { inputAttrs?: any } = {
-  title: "アルバム作成",
-  message: "アルバム名を入力して下さい(後から変更可能です)"
+  title: 'アルバム作成',
+  message: 'アルバム名を入力して下さい(後から変更可能です)',
 };
 
 @Component({
   components: {
-    'entity': Entity,
-    'create-album-prompt': CreateAlbumPrompt
-  }
+    entity: Entity,
+    'create-album-prompt': CreateAlbumPrompt,
+    'add-filled': AddFilledIcon,
+    'fade-transition': FadeTransition,
+  },
 })
 export default class AlbumSelector extends Vue {
-  @Inject("AlbumAPIService") private m_AlbumAPIService!: AlbumAPIService;
+  @Inject('AlbumAPIService') private m_AlbumAPIService!: AlbumAPIService;
 
   public idAndLabelPairs: Array<IDAndLabelPair> = [];
   private isCreateAlbumPromptActive: boolean = false;
@@ -39,13 +58,13 @@ export default class AlbumSelector extends Vue {
   }
 
   public onEntityClicked(id: string): void {
-    this.$router.push({ name: "main-menu", params: { id } });
+    this.$router.push({ name: 'main-menu', params: { id } });
   }
 
   public launchCreateAlbumPrompt() {
     this.$dialog.prompt({
       ...CreateAlbumPromptDefinition,
-      onConfirm: this.onCreateAlbumPromptConfirmed
+      onConfirm: this.onCreateAlbumPromptConfirmed,
     });
   }
 
@@ -53,7 +72,7 @@ export default class AlbumSelector extends Vue {
     let result = await this.m_AlbumAPIService.createAlbumAsync(label);
     this.isCreateAlbumPromptActive = false;
     // TODO: 失敗時はトーストを表示する
-    if(!result) {
+    if (!result) {
       return;
     }
 
--- core/ui/tailwind.config.js
+++ core/ui/tailwind.config.js
@@ -1,11 +1,25 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  purge: ["./public/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  purge: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.blue,
+      },
+      backgroundImage: _theme => ({
+        'seamless-paper': "url('assets/seamless_paper_texture.png')",
+      }),
+      zIndex: {
+        modal: '200',
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      display: ['group-hover'],
+    },
   },
   plugins: [],
 };
